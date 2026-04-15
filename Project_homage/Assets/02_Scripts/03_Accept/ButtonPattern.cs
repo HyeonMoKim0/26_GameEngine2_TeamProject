2ed3222c27e0d7ece24323393efb3e037f44f8db
PoxerPark <76842929+Poxer7777@users.noreply.github.com>
2026-04-15 16:40:23 +0900

    260415_1639 temp01
    동의게임 [점점 커지는 비동의 버튼] 구현

--- Project_homage/Assets/02_Scripts/03_Accept/ButtonPattern.cs
+++ Project_homage/Assets/02_Scripts/03_Accept/ButtonPattern.cs
@@ -5,38 +5,52 @@ using UnityEngine;
 public class ButtonPattern : MonoBehaviour
 {
     public GameObject acceptButton;
-    private RectTransform buttonRect;
+    public GameObject cancelButton;
+    private RectTransform acceptRect;
+    private RectTransform cancelRect;
 
     public float buttonRange = 200;
     public float moveSpeed = 960f;
 
+    bool isButtonPattern1 = false;
+    bool isButtonPattern2 = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        buttonRect = acceptButton.GetComponent<RectTransform>();
+        acceptRect = acceptButton.GetComponent<RectTransform>();
+        cancelRect = cancelButton.GetComponent<RectTransform>();
     }
 
     void KeepButtonInScreen()
     {
-        Vector3 pos = buttonRect.position;
+        Vector3 pos = acceptRect.position;
         pos.x = Mathf.Clamp(pos.x, 0, Screen.width);
         pos.y = Mathf.Clamp(pos.y, 0, Screen.height);
-        buttonRect.position = pos;
+        acceptRect.position = pos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 buttonPos = buttonRect.position;
-
-        float distance = Vector3.Distance(mousePos, buttonPos);
-
-        if (distance < buttonRange)
+        if (isButtonPattern1)
         {
-            Vector3 awayDirection = (buttonPos - mousePos).normalized;
-            buttonRect.position += awayDirection * moveSpeed * Time.deltaTime;
-            KeepButtonInScreen();
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 buttonPos = acceptRect.position;
+
+            float distance = Vector3.Distance(mousePos, buttonPos);
+
+            if (distance < buttonRange)
+            {
+                Vector3 awayDirection = (buttonPos - mousePos).normalized;
+                acceptRect.position += awayDirection * moveSpeed * Time.deltaTime;
+                KeepButtonInScreen();
+            }
+        }
+
+        if (isButtonPattern2)
+        {
+            cancelRect.localScale += Vector3.one * Time.deltaTime;
         }
     }
 }
