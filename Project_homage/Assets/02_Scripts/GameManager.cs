463490697a28e058abc638080e069eab30c4e8ee
PoxerPark <76842929+Poxer7777@users.noreply.github.com>
2026-04-16 03:43:26 +0900

    260416_0343 temp03
    스테이지 진행 구현 임시저장
    일시정지 구현 임시저장

--- Project_homage/Assets/02_Scripts/GameManager.cs
+++ Project_homage/Assets/02_Scripts/GameManager.cs
@@ -7,8 +7,9 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameObject pauseScreen;
     public int lives = 4;
-    public int currentRound;
+    public int currentRound = 0;
 
     private void Awake()
     {
@@ -20,18 +21,21 @@ public class GameManager : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(pauseScreen);
     }
 
-    public void RandomRound()
+    public void RandomRoundStart()
     {
+        currentRound++;
+
         int randomRound = Random.Range(1, 4);
         switch (randomRound)
         {
             case 1:
-                SceneManager.LoadScene("01_Wa Sans");
+                SceneManager.LoadScene("01_WaSans");
                 break;
             case 2:
-                SceneManager.LoadScene("02_Defuse Bomb");
+                SceneManager.LoadScene("02_DefuseBomb");
                 break;
             case 3:
                 SceneManager.LoadScene("03_Accept");
@@ -39,15 +43,55 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void PauseGame()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0; 
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+    }
+
+    public void failedGame()
+    {
+        lives--;
+        if (lives > 0)
+        {
+            RandomRoundStart();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreen.activeSelf)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
     }
 }
