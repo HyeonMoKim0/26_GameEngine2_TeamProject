0e149d6d7275cd172234163648fe3ded290747d6
PoxerPark <76842929+Poxer7777@users.noreply.github.com>
2026-05-19 03:32:37 +0900

    260519_0332 temp20

---
+++ Project_homage/Assets/02_Scripts/05_Rhythm/Rhythm.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rhythm : MonoBehaviour
+{
+    public Image redBall;
+    public Image blueBall;
+
+    float[] rhythmMS = {
+        0.43f, 0.86f, 1.29f,
+        1.72f, 2.15f, 2.58f,
+        3.01f, 3.44f, 3.87f,
+        4.30f, 4.73f, 5.16f,
+        5.59f, 6.02f, 6.45f,
+        6.88f, 7.31f
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
