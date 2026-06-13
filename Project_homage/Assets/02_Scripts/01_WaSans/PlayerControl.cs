75d5fb8df745215e815945efb8dedf64e557a233
PoxerPark <76842929+Poxer7777@users.noreply.github.com>
2026-06-14 01:49:39 +0900

    01_Sans 레이저 이펙트 적용
    260614_0149

--- Project_homage/Assets/02_Scripts/01_WaSans/PlayerControl.cs
+++ Project_homage/Assets/02_Scripts/01_WaSans/PlayerControl.cs
@@ -6,12 +6,15 @@ using UnityEngine.Animations;
 public class PlayerControl : MonoBehaviour
 {
     public GameObject player;
+    public GameObject model;
     CharacterController controller;
 
     float horizontalInput;
     float verticalInput;
     Vector3 move;
     public float moveSpeed;
+
+    public float rotationSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,16 +31,15 @@ public class PlayerControl : MonoBehaviour
         {
             move = transform.right * horizontalInput + transform.forward * verticalInput;
             controller.Move(move * moveSpeed * Time.deltaTime);
-        }
 
-        if (move.magnitude > 0.05f)
-        {
-            Vector3 lookDirection = new Vector3(move.x, 0, move.z);
+            if (move.sqrMagnitude > 0.001f)
+            {
+                // 이동하려는 방향(move)을 바라보는 회전값(Quaternion)을 계산합니다.
+                Quaternion targetRotation = Quaternion.LookRotation(move);
 
-            Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-
-            float rotationSpeed = 15f;
-            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                // 자식 모델링 오브젝트(player)만 현재 회전에서 목표 회전까지 부드럽게(Slerp) 돌려줍니다.
+                model.transform.rotation = Quaternion.Slerp(model.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
